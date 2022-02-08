3dbf18153f78cbc0821209149dce2adc82bdfcbd
URIEL <urielaweobe@gmail.com>
2022-02-08 09:56:45 +0100

    update: complete tutorial

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,44 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:(234)8144521602">(234)8144521602</LinkItem>
+        </LinkColumn>
+
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:urielaweobe@gmail.com">urielaweobe@gmail.com</LinkItem>
+        </LinkColumn>
+        
+      </LinkList>
+      
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Consistency is key</Slogan>
+        </CompanyContainer>
+
+        <SocialContainer>
+        
+          <SocialIcons href='https://github.com/urielaweobe'>
+            <AiFillGithub size='3rem'/>
+          </SocialIcons>
+
+          <SocialIcons href='https://www.linkedin.com/in/uriel-awe-obe/'>
+            <AiFillLinkedin size='3rem'/>
+          </SocialIcons>
+
+          <SocialIcons href='https://instagram.com/urielaweobe'>
+            <AiFillInstagram size='3rem'/>
+          </SocialIcons>
+      
+        </SocialContainer>
+        
+      </SocialIconsContainer>
+
+    </FooterWrapper>
   );
 };
 
