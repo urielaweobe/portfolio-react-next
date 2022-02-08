4678b09111c182f6575613b8af663d5969e63bd4
URIEL <urielaweobe@gmail.com>
2022-02-08 09:01:03 +0100

    update: technology section tutorial

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,48 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range of technologies in the web development world from Design to Front-end and Back-end.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size={'3rem'}/>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      
+      <ListItem>
+        <DiFirebase size={'3rem'}/>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      
+      <ListItem>
+        <DiZend size={'3rem'}/>
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            tools like Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
